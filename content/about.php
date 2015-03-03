762fc546d2e1e2be940568f0bccb386ddbe74b2e
Marty <martin.l.billard@gmail.com>
2015-03-03 10:53:50 +0100

    delete useless folder

--- content/about.php
+++
@@ -1,3 +0,0 @@
-<?php include ("content/includes/header.html"); ?>
-    <h1>About</h1>
-<?php include ("content/includes/footer.html") ?>
--- content/form.php
+++
@@ -1,3 +0,0 @@
-<?php include ("/content/includes/header.html"); ?>
-    <h1>Incoming</h1>
-<?php include ("content/includes/footer.html") ?>
