1cbda49d50739b60eb5a576f39f30e47698ab30e
Marty <martin.l.billard@gmail.com>
2015-03-10 01:36:23 +0100

    Update the home page

--- content/home.php
+++ content/home.php
@@ -1,4 +1,37 @@
 <h1>Banding-Tracking</h1>
-<h4>Outil d'étude des populations d'oiseaux bagués</h4>
+<h4 class="margin-title">Outil d'étude des populations d'oiseaux bagués</h4>
 
-<!-- Add some link to the programs an web site -->
+<div class="row">
+  	<div class="col-sm-6 col-md-4">
+  		<a href="index.php?url=form">
+	    	<div class="thumbnail">
+	      		<img src="statics/pictures/plover_1.jpg" alt="Ajouter une observation">
+	      		<div class="caption">
+			        <h3>Ajouter une observation</h3>
+	      		</div>
+	    	</div>
+	    </a>
+  	</div>
+
+  	<div class="col-sm-6 col-md-4">
+  		<a href="http://www.gonm.org/">
+	    	<div class="thumbnail">
+	      		<img src="statics/pictures/gonm_logo.jpg" alt="Site du GONm">
+	      		<div class="caption">
+			        <h3>Le GONm</h3>
+	      		</div>
+	    	</div>
+	    </a>
+  	</div>
+
+  	<div class="col-sm-6 col-md-4">
+  		<a href="http://crbpo.mnhn.fr/">
+	    	<div class="thumbnail">
+	      		<img src="statics/pictures/mnhn.jpg" alt="Site du GONm">
+	      		<div class="caption">
+			        <h3>Le CRBPO</h3>
+	      		</div>
+	    	</div>
+	    </a>
+  	</div>
+</div>
