c0e716fb0e943b3cbf21970a97cc2d704765a7fe
Marty <martin.l.billard@gmail.com>
2015-03-12 22:02:06 +0100

    Update the home page

--- content/home.php
+++ content/home.php
@@ -2,7 +2,7 @@
 <h4 class="margin-title">Outil d'étude des populations d'oiseaux bagués</h4>
 
 <div class="row">
-  	<div class="col-sm-6">
+  	<div class="col-sm-7">
   		<a href="index.php?url=form">
 	    	<div class="thumbnail">
 	      		<img src="statics/pictures/plover_1.jpg" alt="Ajouter une observation">
@@ -13,25 +13,27 @@
 	    </a>
   	</div>
 
-  	<div class="col-sm-6">
-  		<a href="http://www.gonm.org/">
-	    	<div class="thumbnail">
-	      		<img src="statics/pictures/gonm_logo.jpg" alt="Site du GONm">
-	      		<div class="caption">
-			        <h3>Le GONm</h3>
-	      		</div>
-	    	</div>
-	    </a>
-  	</div>
+	<div class="col-sm-3">
+	  	<div class="col-sm-12">
+	  		<a href="http://www.gonm.org/">
+		    	<div class="thumbnail">
+		      		<img src="statics/pictures/gonm_logo.jpg" alt="Site du GONm">
+		      		<div class="caption">
+				        <h3>Le GONm</h3>
+		      		</div>
+		    	</div>
+		    </a>
+	  	</div>
 
-  	<div class="col-sm-6">
-  		<a href="http://crbpo.mnhn.fr/">
-	    	<div class="thumbnail">
-	      		<img src="statics/pictures/mnhn.jpg" alt="Site du GONm">
-	      		<div class="caption">
-			        <h3>Le CRBPO</h3>
-	      		</div>
-	    	</div>
-	    </a>
-  	</div>
+	  	<div class="col-sm-12">
+	  		<a href="http://crbpo.mnhn.fr/">
+		    	<div class="thumbnail">
+		      		<img src="statics/pictures/mnhn.jpg" alt="Site du GONm">
+		      		<div class="caption">
+				        <h3>Le CRBPO</h3>
+		      		</div>
+		    	</div>
+		    </a>
+	  	</div>
+	</div>
 </div>
