97ab5acc792eb6d4e59fbb475d5503001b4a291c
Marty <martin.l.billard@gmail.com>
2015-03-27 00:49:34 +0100

    Remove useless comments (ref #14)

--- core/pdf_creator.php
+++ core/pdf_creator.php
@@ -23,13 +23,6 @@ if (isset($_POST["bird_infos"]) && isset($_POST["observers_list"])) {
 	echo "<br><br>";
 	print_r($observers_list);
 
-/*	
-	$pdf = new FPDF(); // Create a new PDF file
-	$pdf->AddPage(); // Create a blank page
-	$pdf->SetFont("Arial", "", 12); // Set the font (Font family, style, size)
-	$pdf->Cell(40, 10, "Hello World !");
-	$pdf->Output("test.pdf", "D");*/
-
 	ob_get_clean();
 	class PDF extends FPDF {
 		var $B;
