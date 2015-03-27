5d371bacae7437792d3df4fa571170e109c3d839
Marty <martin.l.billard@gmail.com>
2015-03-27 13:25:29 +0100

    Test the download of a csv file (ref #19)

---
+++ core/export_csv.php
@@ -0,0 +1,16 @@
+<?php
+
+include "database_operations.php";
+$db = new Database_operations(); // create the "request object"
+
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+
+// output the column headings
+fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
+
+?>
