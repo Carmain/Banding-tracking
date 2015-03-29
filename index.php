b1cc5db7eecc5a0676424baab0d547d148709fd1
Marty <martin.l.billard@gmail.com>
2015-03-29 11:47:47 +0200

    Remove the about page

--- index.php
+++ index.php
@@ -34,7 +34,6 @@
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
                         <li><a href="index.php?url=form">Vous avez vu un gravelot ?</a></li>
-                        <li><a href="index.php?url=about">Informations complémentaires</a></li>
                     </ul>
                 </div>
             </div>
