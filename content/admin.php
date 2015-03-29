c37e8d5eb34adc14f2f1e5e586aa6a495bd17650
Marty <martin.l.billard@gmail.com>
2015-03-29 16:48:29 +0200

    BUGFIX : Error during the export of the CSV

--- content/admin.php
+++ content/admin.php
@@ -26,24 +26,7 @@ if (isset($_SESSION["username"]) && isset($_SESSION["id"])) { ?>
 
 	<?php
 	if (isset($_POST["csv"])) {
-		// output headers so that the file is downloaded rather than displayed
-		header('Content-Type: text/csv; charset=utf-8');
-		header('Content-Disposition: attachment; filename=lectures.csv');
-
-		// create a file pointer connected to the output stream
-		$output = fopen('php://output', 'w');
-
-		// output the column headings
-		fputcsv($output, array('Année du baguage', 'Date du baguage', 'Bague métal', 
-							   'Numéro', 'Couleur', 'Sexe', 'Âge au baguage', 'Date de lecture', 
-							   'Nom du lecteur', 'Ville de lecture', 'Département de lecture', 
-							   'Lieu-dit de lecture'));
-
-		$data_observations = $db->save_observations();
-		foreach ($data_observations as $row) {
-			fputcsv($output, $row);
-		}
-		fpassthru($output);
+		header("Location: core/export_csv.php");
 	}
 }
 
