44f78cd2db5ae170f6ca165cb4b5737704b65cc4
Marty <martin.l.billard@gmail.com>
2015-04-23 14:35:45 +0200

    Update the structure of the database

--- database_struct/gonm.sql
+++ database_struct/gonm.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 3.4.11.1deb2+deb7u1
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Dim 29 Mars 2015 à 17:56
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Client: localhost
+-- Généré le: Jeu 23 Avril 2015 à 14:34
+-- Version du serveur: 5.5.41
+-- Version de PHP: 5.4.36-0+deb7u3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `gonm`
+-- Base de données: `gonm`
 --
 
 -- --------------------------------------------------------
@@ -28,23 +28,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `kentish_plover` (
   `id_kentish_plover` int(11) NOT NULL AUTO_INCREMENT,
-  `year` year(4) NOT NULL,
-  `banding_year` year(4) NOT NULL,
-  `action` varchar(10) NOT NULL,
-  `metal_ring` varchar(20) NOT NULL,
-  `number` int(11) NOT NULL,
-  `color` varchar(20) DEFAULT NULL,
-  `sex` varchar(1) NOT NULL,
-  `age` varchar(3) NOT NULL,
-  `date` varchar(10) NOT NULL,
+  `year` int(11) DEFAULT NULL,
+  `banding_year` int(11) DEFAULT NULL,
+  `action` varchar(6) DEFAULT NULL,
+  `metal_ring` varchar(12) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `color` varchar(12) DEFAULT NULL,
+  `sex` varchar(4) DEFAULT NULL,
+  `age` varchar(3) DEFAULT NULL,
+  `date` varchar(10) DEFAULT NULL,
   `banding_time` varchar(5) DEFAULT NULL,
-  `town` varchar(27) NOT NULL,
-  `department` varchar(2) NOT NULL,
-  `locality` varchar(100) DEFAULT NULL,
-  `bander` varchar(5) NOT NULL,
-  `observer` varchar(30) DEFAULT NULL,
+  `town` varchar(27) DEFAULT NULL,
+  `department` int(11) DEFAULT NULL,
+  `locality` varchar(30) DEFAULT NULL,
+  `bander` varchar(7) DEFAULT NULL,
+  `observer` varchar(13) DEFAULT NULL,
+  `first_name_observer` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`id_kentish_plover`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=797 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=799 ;
 
 -- --------------------------------------------------------
 
@@ -59,11 +60,11 @@ CREATE TABLE IF NOT EXISTS `observations` (
   `first_name` varchar(200) NOT NULL,
   `date` date NOT NULL,
   `town` varchar(250) NOT NULL,
-  `department` varchar(250) NOT NULL,
-  `locality` varchar(200) NOT NULL,
+  `department` int(11) NOT NULL,
+  `locality` varchar(200) DEFAULT NULL,
   `sex` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_observations`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=156 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4318 ;
 
 -- --------------------------------------------------------
 
