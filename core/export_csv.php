3617c96b3c4dbf8dbde01bb771d0ec40ab7c7893
Billard Martin <martin.l.billard@gmail.com>
2015-05-19 11:02:21 +0200

    Add columns title for the export

--- core/export_csv.php
+++ core/export_csv.php
@@ -10,10 +10,10 @@ header('Content-Disposition: attachment; filename=lectures.csv');
 $output = fopen('php://output', 'w');
 
 // output the column headings
-fputcsv($output, array('Année du baguage', 'Date du baguage', 'Bague métal', 
-					   'Numéro', 'Couleur', 'Sexe', 'Âge au baguage', 'Date de lecture', 
-					   'Nom du lecteur', 'Ville de lecture', 'Département de lecture', 
-					   'Lieu-dit de lecture'));
+fputcsv($output, array('Année du baguage', 'Date du baguage', 'Bague métal',
+					   'Numéro', 'Couleur', 'Sexe', 'Âge au baguage', 'Date de lecture',
+					   'Nom du lecteur', 'Ville de lecture', 'Département de lecture',
+					   'Lieu-dit de lecture', 'Coordonnée X', 'Coordonnée Y', 'Commentaires'));
 
 $data_observations = $db->csv_save();
 foreach ($data_observations as $row) {
